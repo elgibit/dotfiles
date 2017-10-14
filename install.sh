3c5606d824aa17fccbaa72fcd7e1dac661556e3f
Łukasz Garboś <lukasz.garbos@gmail.com>
2017-10-14 13:04:20 +0200

    Install parameters

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+gitfile=~/.gitconfig
+
+if [ $# -ne 2 ]; then
+    echo $0: use params: name email
+    exit 1
+fi
+
 cp .bash_profile ~/.bash_profile
 cp .bash_prompt ~/.bash_prompt
 cp .gitconfig ~/.gitconfig
+
+echo "  name = $1" >> $gitfile
+echo "  email = $2" >> $gitfile
 
 source ~/.bash_profile
